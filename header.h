86be5184283980cc77b437a5f5046d8eb9cd399e
TomasRay <tomasvaiciunas@live.com>
2022-10-29 22:10:58 +0000

    Create header.h

---
+++ header.h
@@ -0,0 +1,27 @@
+
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <sstream>
+#include <algorithm>
+#include <fstream>
+
+using std::cout;
+using std::cout;
+using std::cin;
+using std::setprecision;
+using std::setw;
+using std::left;
+using std::right;
+using std::string;
+using std::vector;
+using std::endl;
+using std::istream;
+using std::ostream;
+
+#include "Class.h"
+
+
